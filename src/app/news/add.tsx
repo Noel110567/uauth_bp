5fdb8301bd0e4eeefbd6b0098fa66de28b067f9c
Noel110567 <lotto110567@gmail.com>
2025-09-04 13:42:09 +0800

    modified add.tsx

--- src/app/news/add.tsx
+++ src/app/news/add.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useRef } from "react";
 import { useRouter } from "next/navigation";
-import NewsFullStoryEditor from "@/components/NewsFullStoryEditor";
+import NewsFullStoryEditor from "../../components/NewsFullStoryEditor";
 
   type NewsItem = {
     title: string;
